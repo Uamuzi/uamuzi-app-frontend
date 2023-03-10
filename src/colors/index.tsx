28cf7180621355ff772c1ad8eb718ba02a826ade
Grayce Muthui <graycemuthui20@gmail.com>
2023-03-10 17:10:57 +0300

    Add assets and components

---
+++ src/colors/index.tsx
@@ -0,0 +1,13 @@
+export const uamuziPrimary = "#9400ff";
+export const uamuziSecondary = "#8364ac";
+export const uamuziDull = "#a0a09f";
+export const uamuziLight = "#F0F8FF";
+export const uamuziLightGray = "#e0e0e0";
+export const uamuziRed = "#D22B2B";
+export const uamuziLightBlue = "#78C7C7";
+
+// Redesign colors
+export const uamuziPurple = "#8067AD";
+export const uamuziGrey = "#A0A09F";
+export const uamuziMatte = "#333";
+export const uamuziWhite = "white";
